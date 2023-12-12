05585b096635e5c1156a2ff83fd7b0b3d5cf93c0
Wafaa HIJARY <95526772+Wafwaf14@users.noreply.github.com>
2023-12-12 12:20:40 +0100

    tests

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+let's test the user class
+"""
+import unittest
+
+
+class TestUser(unittest.TestCase):
+    """Test class"""
+
+
+if __name__ == "__main__":
+    unittest.main()
